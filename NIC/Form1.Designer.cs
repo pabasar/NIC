19a83c43adf43e20f11365829eb093e1aece1ca1
Pabasar <Pabasar@MSI>
2022-05-03 12:37:52 +0530

    Add project files.

---
+++ NIC/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace NIC
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.txtGender = new System.Windows.Forms.TextBox();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.txtVote = new System.Windows.Forms.TextBox();
+            this.txtNIC = new NICTextBox.NICTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 32);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "NIC Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gender";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 156);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date of Birth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(88, 184);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Vote Eligibility";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(128, 68);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(86, 31);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next Person";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // txtGender
+            // 
+            this.txtGender.Enabled = false;
+            this.txtGender.Location = new System.Drawing.Point(170, 123);
+            this.txtGender.Margin = new System.Windows.Forms.Padding(2);
+            this.txtGender.Name = "txtGender";
+            this.txtGender.Size = new System.Drawing.Size(91, 20);
+            this.txtGender.TabIndex = 6;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Enabled = false;
+            this.txtDOB.Location = new System.Drawing.Point(170, 154);
+            this.txtDOB.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(91, 20);
+            this.txtDOB.TabIndex = 7;
+            // 
+            // txtVote
+            // 
+            this.txtVote.Enabled = false;
+            this.txtVote.Location = new System.Drawing.Point(170, 181);
+            this.txtVote.Margin = new System.Windows.Forms.Padding(2);
+            this.txtVote.Name = "txtVote";
+            this.txtVote.Size = new System.Drawing.Size(91, 20);
+            this.txtVote.TabIndex = 8;
+            // 
+            // txtNIC
+            // 
+            this.txtNIC.BackColor = System.Drawing.Color.LightYellow;
+            this.txtNIC.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.txtNIC.Location = new System.Drawing.Point(170, 29);
+            this.txtNIC.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNIC.Name = "txtNIC";
+            this.txtNIC.Size = new System.Drawing.Size(91, 20);
+            this.txtNIC.TabIndex = 9;
+            this.txtNIC.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Enter_KeyUp);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 240);
+            this.Controls.Add(this.txtNIC);
+            this.Controls.Add(this.txtVote);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtGender);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frmMain";
+            this.Text = "NIC - Personal Deatils";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.TextBox txtGender;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.TextBox txtVote;
+        private NICTextBox.NICTextBox txtNIC;
+    }
+}
+
---
+++ NIC/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NICLibrary;
+
+namespace NIC
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        int numOfPersons = 0;
+
+        string nicNumber,gndr,yr,vt;
+
+        NICLibrary.NICOperations nl= new NICLibrary.NICOperations();
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            txtNIC.Text = "";
+            txtGender.Text = "";
+            txtDOB.Text = "";
+            txtVote.Text = "";
+
+            txtNIC.Focus();
+            btnNext.Enabled = false;
+
+            numOfPersons++;
+        }
+
+        
+
+        private void Enter_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+
+            if (e.KeyCode == Keys.Enter && txtNIC.Text != "")
+            {
+                nicNumber = txtNIC.Text;
+
+                if(nicNumber.Length == 10 || nicNumber.Length == 12)
+                {
+                    nicNumber = nl.NICFormat(nicNumber);
+                    gndr = nl.GenderCheck(nicNumber);
+                    vt = nl.VoteCheck(nicNumber);
+                    yr = nl.DayCheck(nicNumber);
+
+                    txtGender.Text = gndr;
+                    txtDOB.Text = yr;
+                    txtVote.Text = vt;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid NIC!");
+                }          
+
+                btnNext.Enabled = true;
+                btnNext.Focus();
+
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnNext.PerformClick();
+        }
+    }
+}
